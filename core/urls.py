184806da27478c649dfa483043f13b8eec5fd0ce
Ahmed Lemine <ahmed.lemine@yahoo.com>
2024-03-19 17:48:28 +0400

    added rest auth(jwt) via djoser, new accounts app

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
     path("api/v1/", include("merger.urls", namespace="merger")),
     path("admin/", admin.site.urls),
 ]
