e9af95787ae59ab509da926d7a71b2d8e3047fda
Ahmed Lemine <ahmed.lemine@yahoo.com>
2024-08-02 00:43:04 +0400

    fixed bug: merger context doesn't cloes

--- merger/utils.py
+++ merger/utils.py
@@ -7,31 +7,29 @@ from django.conf import settings
 
 from .exceptions import MergeException
 
-merger = PdfWriter()
-
-
 def merge_pdf_files(pdf_files):
     """
     merges a list of pdf files using pypdf and returns the path
     for the merged PDF file.
     :param list pdf_files: a list of PDF files to merge.
     """
-
-    with merger:
-        for f in pdf_files:
-            try:
-                merger.append(f.file)
-            except FileNotFoundError:
-                raise MergeException
-
-        merged_path = os.path.join(
-            settings.MEDIA_ROOT, f"merged_pdfs/merged_pdf_{uuid.uuid4()}.pdf"
-        )
-
+    merger = PdfWriter()
+    for f in pdf_files:
         try:
-            merger.write(merged_path)
-        except (FileNotFoundError, PdfReadError, PyPdfError):
+            merger.append(f.file)
+        except FileNotFoundError:
             raise MergeException
+
+    merged_path = os.path.join(
+        settings.MEDIA_ROOT, f"merged_pdfs/merged_pdf_{uuid.uuid4()}.pdf"
+    )
+
+    try:
+        merger.write(merged_path)
+    except (FileNotFoundError, PdfReadError, PyPdfError):
+        raise MergeException
+    
+    merger.close()
 
     if os.path.isfile(merged_path):
         return merged_path
